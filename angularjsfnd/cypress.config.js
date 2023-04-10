4a0a99de6070a779a62d7ad38522ed9a438a09bc
Ronald Chan <the@gr8.ninja>
2023-04-10 11:38:06 +0800

    make angularjsfnd coverage work

--- angularjsfnd/cypress.config.js
+++ angularjsfnd/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   startCommand: 'yarn start:coverage',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('../cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:8000',
     specPattern: 'cypress/e2e/angularjsfnd/**/*',
